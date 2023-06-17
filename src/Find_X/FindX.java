e50d4b30eda279b23f74c9d33e506f719e44b29c
sgc93 <smachewgedefaw@gmail.com>
2023-06-16 18:13:44 -0900

    a solution finder class for 3 cases

---
+++ src/Find_X/FindX.java
@@ -0,0 +1,36 @@
+package Find_X;
+
+public class FindX implements Runnable {
+    double a, b, c;
+    double discriminant;
+
+    public FindX(double a, double b, double c, double discriminant) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.discriminant = discriminant;
+    }
+
+    @Override
+    public void run() {
+        if (discriminant > 0) {
+            double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            System.out.println("\033[032m");
+            System.out.println("The equation has two distinct real solutions:");
+            System.out.println("x1 = " + x1);
+            System.out.println("x2 = " + x2);
+            System.out.println("\033[0m");
+        } else if (discriminant == 0) {
+            double x = -b / (2 * a);
+            System.out.println("\033[032m");
+            System.out.println("The equation has one real solution:");
+            System.out.println("x = " + x);
+            System.out.println("\033[0m");
+        } else {
+            System.out.println("\033[033m");
+            System.out.println("The equation has no real solutions.");
+            System.out.println("\033[0m");
+        }
+    }
+}
