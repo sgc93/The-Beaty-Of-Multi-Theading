331290beffe871764371b8a89cb7fad0a670066b
sgc93 <smachewgedefaw@gmail.com>
2023-06-16 15:54:46 -0900

    check for the validator

---
+++ src/The_Validators/isValid.java
@@ -0,0 +1,22 @@
+package The_Validators;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class isValid extends Thread{
+    String token;
+    String only_alphaBets = "^[a-zA-Z_]+$";
+    Pattern pattern = Pattern.compile(only_alphaBets);
+    public isValid(String token) {
+        this.token = token;
+    }
+
+    public void run(){
+        Matcher matcher = pattern.matcher(token);
+        if (!matcher.matches()) {
+            System.out.println("your user name should contain only alphabets and underscore -> [a,z] + [A,Z] + _ ");
+            System.exit(0);
+        }
+    }
+    
+}
