2623f121e1fffc9682831c36f6bd5e516f836dcb
sgc93 <smachewgedefaw@gmail.com>
2023-06-16 15:55:28 -0900

    check for the 0 length of the user input

---
+++ src/The_Validators/IsEmpty.java
@@ -0,0 +1,17 @@
+package The_Validators;
+
+public class IsEmpty extends Thread{
+    String token;
+    
+    public IsEmpty(String token){
+        this.token = token;
+    }
+
+    public void run(){
+        if(token.length() == 0){
+            System.out.println("\033[31m");
+            System.out.println("   your Have entered empty values!");
+            System.exit(0);
+        }
+    }
+}
