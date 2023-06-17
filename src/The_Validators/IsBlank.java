1249f8fc10b47886556513ced1e6a06b785cd431
sgc93 <smachewgedefaw@gmail.com>
2023-06-16 15:53:54 -0900

    check for empityness of the user input

---
+++ src/The_Validators/IsBlank.java
@@ -0,0 +1,17 @@
+package The_Validators;
+
+public class IsBlank extends Thread{
+    String token;
+
+    public IsBlank(String token){
+        this.token = token;
+    }
+
+    public void run(){
+        if(token.isBlank()){
+            System.out.println("\033[31m");
+            System.out.println("    Attempting to use only Blank spaces is not allowed!, sorry try again please");
+            System.exit(0);
+        }
+    }
+}
