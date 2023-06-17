8b87b33aed14abef21314c352f17a198232a2cd9
sgc93 <smachewgedefaw@gmail.com>
2023-06-16 15:54:22 -0900

    checker for strength of the user input

---
+++ src/The_Validators/IsStrong.java
@@ -0,0 +1,26 @@
+package The_Validators;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class IsStrong extends Thread{
+    String token;
+    String uppercases = ".*[A-Z].*";
+    Pattern pattern = Pattern.compile(uppercases);
+    public IsStrong(String token){
+        this.token = token;
+    }
+    
+    public void run(){
+        Matcher matcher = pattern.matcher(token);
+        if(token.length() < 5){
+            System.out.println("\033[31m");
+            System.out.println("    Your username must have at least 5 containers!");
+            System.exit(0);
+        } else if (!matcher.matches()) {
+            System.out.println("\033[31m");
+            System.out.println("Your username should contain at least one uppercase alphabet.");
+            System.exit(0);
+        }
+    }
+}
